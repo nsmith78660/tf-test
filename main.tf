9bea247e196778c96c702d730ac1116e99c75fa8
Nathanael Smith <nsmith78660@gmail.com>
2024-02-13 17:21:22 -0600

    commit2

--- main.tf
+++ main.tf
@@ -22,14 +22,10 @@ terraform {
 
 
 # Variable blocks directly within the main.tf. No arguments necessary.
-# variable "aws_access_key" {}
-# variable "aws_secret_key" {}
 variable "region" {}
 
 # provider arguments call on the variables which then call on terraform.tfvars for the values.
 provider "aws" {
-  # access_key = var.aws_access_key
-  # secret_key = var.aws_secret_key
   region     = "us-east-2"
 }
 
@@ -42,4 +38,8 @@ resource "aws_instance" "tc_instance" {
   tags = {
     Name = "TC-triggered-instance"
   }
+}
+
+resource "aws_iam_user" "test_user_99" {
+  name = "test-user-99"
 }
